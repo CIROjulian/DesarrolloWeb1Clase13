4a279263d7285cf34658389d4fb1fe377aa77449
CIROjulian <127125051+CIROjulian@users.noreply.github.com>
2025-03-18 19:19:28 -0500

    se crea la dto para los estudiantes

---
+++ Dto/StudentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesarrolloWeb1Clase13.Dto
+{
+    public class StudentDto
+    {
+        public string Identificacion { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+
+
+    }
+}
